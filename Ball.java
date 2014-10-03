3b6f66c2bb5c3e5278022736aff1b5e421c4d258
Peter Lorenz <peters_kotlett@hotmail.com>
2014-10-03 13:51:13 +0200

    Create Ball.java

---
+++ Ball.java
@@ -0,0 +1,34 @@
+package tls;
+/**
+ *
+ * @author Peter
+ */
+public class Ball {
+  private int size;
+  private String color;
+  private int air_pressure = 20; 
+  private boolean is_flat = false;  //is the ball flat yes/no?
+  
+  public Ball(int initSize, String initColor) {
+    size = initSize;
+    color = initColor; 
+  }
+  public void bounce(){
+    if(is_flat)
+      System.out.println("Sorry, the ball is so flat, so it cannot bounce.");
+    else
+      System.out.println("The " + color + " is bouncing.");
+    
+  }
+  
+  public void deflate() {
+    air_pressure = 0;
+    is_flat = true;
+  }
+  
+  public void inflate() {
+    air_pressure = 20;
+    is_flat = false;
+  }
+    
+}
